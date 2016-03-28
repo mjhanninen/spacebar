0c78b41de8cef41cbe8625c3cc03669cf68018c1
Matti Hänninen <matti@mjhanninen.com>
2016-03-28 20:48:46 +0300

    First prototype version of the game

--- proto/spacebar/main.lua
+++ proto/spacebar/main.lua
@@ -1,11 +1,60 @@
 function love.load()
-    -- Kutsutaan kerran pelin käynnistyessä
+    aika_laskuri = nil
+    odota_nostoa = false
+    peli_on_loppu = false
+    spacebar_laskuri = 2 + 3 * love.math.random()
+    spacebar_on_lauennut = false
 end
 
-function love.udpate(dt)
-    -- Pelin tilan päivitys tapahtuu täällä
+function love.update(dt)
+    if not peli_on_loppu then
+        if not spacebar_on_lauennut then
+            spacebar_laskuri = spacebar_laskuri - dt
+            if spacebar_laskuri < 0 then
+                spacebar_on_lauennut = true
+                aika_laskuri = 0.4
+            end
+        else
+            aika_laskuri = aika_laskuri - dt
+            if aika_laskuri < 0 then
+                peli_on_loppu = true
+                odota_nostoa = false
+            end
+        end
+    end
 end
 
 function love.draw(dt)
-    -- Pelin ruudun piirrustus tapahtuu täällä
+    love.graphics.clear(25, 75, 230)
+    love.graphics.setColor(255, 115, 25)
+    if peli_on_loppu then
+        love.graphics.print("HÄVISIT!", 300, 275, 0, 4)
+        love.graphics.print("Lopeta painamalla ESC-näppäintä", 295, 350)
+    elseif spacebar_on_lauennut then
+        love.graphics.rectangle("fill", 200, 250, 400, 100)
+    else
+        -- Odotellessa ei näytetä mitään
+    end
+end
+
+function love.keypressed(nappain, scan_koodi, on_toisto)
+    if nappain == "escape" then
+        love.event.quit()
+    elseif not peli_on_loppu then
+        if nappain == "space" then
+            if spacebar_on_lauennut then
+                odota_nostoa = true
+            else
+                peli_on_loppu = true
+            end
+        end
+    end
+end
+
+function love.keyreleased(nappain, scan_koodi, on_toisto)
+    if nappain == "space" and odota_nostoa then
+        odota_nostoa = false
+        spacebar_on_lauennut = false
+        spacebar_laskuri = 2 + 3 * love.math.random()
+    end
 end
