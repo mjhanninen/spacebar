91ddc84e8881e1d68e008415ec70531aef0c884d
Matti Hänninen <matti@mjhanninen.com>
2016-03-28 18:25:27 +0300

    Early stage doodling

---
+++ proto/spacebar/main.lua
@@ -0,0 +1,11 @@
+function love.load()
+    -- Kutsutaan kerran pelin käynnistyessä
+end
+
+function love.udpate(dt)
+    -- Pelin tilan päivitys tapahtuu täällä
+end
+
+function love.draw(dt)
+    -- Pelin ruudun piirrustus tapahtuu täällä
+end
